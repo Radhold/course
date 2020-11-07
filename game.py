dfd20e4eb04fe632dc94ce5fa11aaa013e312a9d
radhold <fomenko.yaroslav@gmail.com>
2020-11-08 02:33:01 +0300

    Облака

--- game.py
+++ game.py
@@ -241,7 +241,7 @@ def gameplay():
                     last_obstacle.empty()
                     last_obstacle.add(Coin(gameSpeed, 30, 30))
 
-        if len(clouds) < 5 and random.randrange(0, 300) == 10:
+        if len(clouds) < 5 and random.randrange(0, 300) == 15:
             Cloud()
 
         clouds.update()
